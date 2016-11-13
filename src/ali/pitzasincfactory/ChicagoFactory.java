a2dccb77f61782fb0696340419ff06cbbbd46cda
Ali Ghahraei Figueroa <aligf94@gmail.com>
2016-11-13 11:36:25 -0600

    Add cookPizza to ChicagoFactory to be able to inherit

--- src/ali/pitzasincfactory/ChicagoFactory.java
+++ src/ali/pitzasincfactory/ChicagoFactory.java
@@ -18,7 +18,19 @@ public class ChicagoFactory extends Factory {
   //
   // Methods
   //
-
+  @Override
+  public Pizza cookPizza(String pizza) {
+    switch (pizza){
+      case "hawaiian":
+        return new ChicagoHawaiianPizza();
+      case "pepperoni":
+        return new ChicagoHawaiianPizza();
+      case "veggie":
+        return new ChicagoVeggiePizza();
+      default:
+        return null;
+    }
+  }
 
   //
   // Accessor methods
