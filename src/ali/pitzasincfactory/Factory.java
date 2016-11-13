d191e2f42b08ee187058a69859cbfb6df6a12b27
Ali Ghahraei Figueroa <aligf94@gmail.com>
2016-11-13 11:37:34 -0600

    Change Factory to abstract and add cookPizza and preparePizza

--- src/ali/pitzasincfactory/Factory.java
+++ src/ali/pitzasincfactory/Factory.java
@@ -3,7 +3,7 @@ package ali.pitzasincfactory;
 /**
  * Class Factory
  */
-public class Factory {
+abstract public class Factory {
 
   //
   // Fields
@@ -18,7 +18,17 @@ public class Factory {
   //
   // Methods
   //
+  public void preparePizza(String pizza){
+    System.out.println("Preparing " + cookPizza(pizza));
+    System.out.println("Preparing the dough");
+    System.out.println("Adding the sauce");
+    System.out.println("Adding ingredients");
+    System.out.println("Cooking in the oven 25 mins at 180 degrees");
+    System.out.println("Cutting the pizza in slices");
+    System.out.println("Putting the pizza in its box");
+  }
 
+  public abstract Pizza cookPizza(String pizza);
 
   //
   // Accessor methods
